45250ca07f7434669c3e7a01f9dbbe946ed01192
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2019-11-06 17:11:59 -0800

    Fuller hydration

--- src/AbstractRepository.php
+++ src/AbstractRepository.php
@@ -5,6 +5,7 @@ namespace Hiraeth\Doctrine;
 use Doctrine\ORM\Query;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
 use Doctrine\ORM\EntityManagerInterface as EntityManager;
 use Doctrine\Common\Collections;
 use Doctrine\DBAL\Types\Type;
@@ -90,7 +91,7 @@ abstract class AbstractRepository extends EntityRepository
 
 
 	/**
- 	 *
+	 *
 	 */
 	public function find($id, $lock_mode = NULL, $lock_version = NULL)
 	{
@@ -307,6 +308,83 @@ abstract class AbstractRepository extends EntityRepository
 	 */
 	protected function updateAssociations(object $object, array $data):  AbstractRepository
 	{
+		$meta_data = $this->getClassMetaData();
+
+		foreach ($data as $field => $value) {
+			if (array_intersect(['*', $field], $object::$_protect ?? ['*'])) {
+				continue;
+			}
+
+			if (array_key_exists($field, $meta_data->associationMappings)) {
+				$mapping = $meta_data->associationMappings[$field];
+
+				switch ($mapping['type']) {
+					case ClassMetadataInfo::ONE_TO_ONE:
+					case ClassMetadataInfo::MANY_TO_ONE:
+						$this->updateAssociationToOne($object, $field, $mapping, $value);
+						break;
+
+					case ClassMetadataInfo::ONE_TO_MANY:
+					case ClassMetadataInfo::MANY_TO_MANY:
+						$this->updateAssociationToMany($object, $field, $mapping, $value);
+						break;
+
+					default:
+						throw new RuntimeException(sprintf(
+							'Unknown mapping type "%s"',
+							$mapping['type']
+						));
+				}
+			}
+		}
+
+		return $this;
+	}
+
+
+	/**
+	 *
+	 */
+	protected function updateAssociationToMany(object $object, string $field, array $mapping, $values):  AbstractRepository
+	{
+		$collection = new Collections\ArrayCollection();
+
+		if (!empty($mapping)) {
+			settype($values, 'array');
+
+			foreach ($values as $value) {
+				$related_entity = $this->getEntityManager()->find($mapping['targetEntity'], $value);
+
+				if ($related_entity) {
+					if (is_array($value)) {
+						$this->update($related_entity, $value);
+					}
+
+					$collection->add($related_entity);
+				}
+			}
+
+			$this->updateProperty($object, $field, $collection);
+		}
+
+		return $this;
+	}
+
+
+	/**
+	 *
+	 */
+	protected function updateAssociationToOne(object $object, string $field, array $mapping, $value):  AbstractRepository
+	{
+		if (!empty($mapping)) {
+			$related_entity = $this->getEntityManager()->find($mapping['targetEntity'], $value);
+
+			if (is_array($value)) {
+				$this->update($related_entity, $value);
+			}
+
+			$this->updateProperty($object, $field, $related_entity);
+		}
 
 		return $this;
 	}
@@ -328,8 +406,11 @@ abstract class AbstractRepository extends EntityRepository
 			}
 
 			if (array_key_exists($full_field, $meta_data->fieldMappings)) {
-				$type  = Type::getType($meta_data->fieldMappings[$full_field]['type'] ?? 'string');
-				$value = $type->convertToPHPValue($value, $platform);
+				if (is_scalar($value)) {
+					$type  = Type::getType($meta_data->fieldMappings[$full_field]['type'] ?? 'string');
+					$value = $type->convertToPHPValue($value, $platform);
+
+				}
 
 				$this->updateProperty($object, $field, $value);
 			}
@@ -354,11 +435,28 @@ abstract class AbstractRepository extends EntityRepository
 	/**
 	 *
 	 */
-	protected function updateProperty(object $object, $name, $value): AbstractRepository
+	protected function updateProperty(object $object, string $name, $value): AbstractRepository
 	{
 		$property = $this->reflectProperty($object, $name);
 
-		$property->setValue($object, $value);
+		if ($value instanceof Collections\Collection) {
+			$collection = $property->getValue($name);
+
+			foreach ($collection as $i => $entity) {
+				if (!$value->contains($entity)) {
+					$collection->remove($i);
+				}
+			}
+
+			foreach ($value as $entity) {
+				if (!$collection->contains($entity)) {
+					$collection->add($entity);
+				}
+			}
+
+		} else {
+			$property->setValue($object, $value);
+		}
 
 		return $this;
 	}
