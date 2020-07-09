44b3ee2afedb8676d726107b3c69468c406c0df7
Vadim Beliak <v8181b@gmail.com>
2020-07-09 16:09:33 -0400

    Fix hydrator: nested embeddables

--- src/Hydrator.php
+++ src/Hydrator.php
@@ -61,7 +61,7 @@ class Hydrator
 			}
 
 			if (array_key_exists($field, $meta_data->embeddedClasses)) {
-				$embeddable = $this->reflectProperty($entity, $field)->getValue($entity);
+				$embeddable = $this->getProperty($entity, $field);
 
 				if (!$embeddable) {
 					$embeddable = new $meta_data->embeddedClasses[$field]['class']();
@@ -263,6 +263,29 @@ class Hydrator
 		}
 
 		return new $target();
+	}
+
+
+	/**
+     * Find the property of a field.
+     */
+    public function getProperty($entity, $field)
+    {
+        $prop = NULL;
+
+        if (strpos($field, '.')) {
+            $parts = explode('.', $field);
+
+            foreach ($parts as $part) {
+                if (!property_exists($entity, $part)) {
+                    $prop = $this->reflectProperty($entity, $part)->getValue($entity);
+                }
+            }
+        } else {
+            $prop = $this->reflectProperty($entity, $field)->getValue($entity);
+        }
+
+        return $prop;
 	}
 
 
