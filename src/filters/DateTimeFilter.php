d1fc30760a1007a3ba5d78c7e3e48732d41a124a
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2021-01-14 11:03:57 -0800

    Enable year only date inputs

--- src/filters/DateTimeFilter.php
+++ src/filters/DateTimeFilter.php
@@ -9,9 +9,22 @@ class DateTimeFilter
 	public function __invoke($value)
 	{
 		if ($value) {
+			//
+			// Allow for accepting year-only strings
+			//
+
+			if (is_string($value) && is_numeric($value) && strlen($value) == 2) {
+				$value = sprintf('01/01/%s', $value);
+			}
+
 			try {
 				return new DateTime($value);
-			} catch (\Exception $e) {}
+
+			} catch (\Exception $e) {
+				//
+				// If the parsing fails we'll hit the NULl return below
+				//
+			}
 		}
 
 		return NULL;
